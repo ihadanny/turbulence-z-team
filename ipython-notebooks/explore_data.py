9e2773c188decd38636e09008bd8011befb4065f
ihadanny <ido.hadanny@gmail.com>
2015-09-12 19:40:17 +0300

    added feature values

--- ipython-notebooks/explore_data.py
+++ ipython-notebooks/explore_data.py
@@ -16,8 +16,14 @@ feature_names = df[["form_name", "feature_name"]].drop_duplicates()
 feature_names.to_csv('../feature_names.csv', sep='|', index=False)
 
 
-# In[ ]:
+# In[18]:
 
 feature_values = df[["form_name", "feature_name", "feature_value"]].drop_duplicates()
-feature_names.to_csv('../feature_names.csv', sep='|', index=False)
+feature_values[np.isnan(feature_values.feature_value.convert_objects(convert_numeric=True))]
+feature_values.to_csv('../feature_values.csv', sep='|', index=False)
+
+
+# In[ ]:
+
+
 
